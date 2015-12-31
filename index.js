42b458c97753bf0847df4e5917b733fa2c529987
Stanley Stuart <stanley@stan.li>
2015-12-31 10:46:51 -0600

    add utility for generating the project-version-thing compound id

--- index.js
+++ index.js
@@ -4,22 +4,27 @@ var _ = require('lodash')
 var fetch = require('./lib/fetch')
 var readDocs = require('./lib/read-docs')
 var addSinceTags = require('./lib/add-since-tags')
+var putClassesInCouch = require('./lib/classes-in-couch')
+var PouchDB = require('pouchdb')
+var createVersionIndex = require('./lib/create-version-index')
+
 
 fetch()
   .then(readDocs)
+  .then(createVersionIndex)
   .then(function (versions) {
     addSinceTags(versions)
 
     var PouchDB = require('pouchdb')
 
-    var db = new PouchDB('http://localhost:5984/ember')
+    var db = new PouchDB('http://localhost:5984/project-versions')
 
     var tojsonapi = require('yuidoc-to-jsonapi/lib/converter')
 
     var jsonapidocs = versions.map(function (version) {
       var jsonapidoc = tojsonapi(version.data)
+
       // now that we have one giant ass document, put it on a diet to something smaller.
-      //
       var projectData = {
         type: 'project',
         id: 'ember',
@@ -32,21 +37,21 @@ fetch()
         _id: version.version,
         data: {
           id: version.version,
-          type: 'project_version'
-        },
-        relationships: {
-          classes: {
-            data: jsonapidoc.data.filter(item => item.type === 'class').map(item => ({id: item.id, type: 'class'}))
-          },
-          modules: {
-            data: jsonapidoc.data.filter(item => item.type === 'module').map(item => ({id: item.id, type: 'module'}))
-          },
-          project: {
-            data: {
-              id: 'ember',
-              type: 'project'
+          type: 'project-version',
+          relationships: {
+            classes: {
+              data: jsonapidoc.data.filter(item => item.type === 'class').map(item => ({id: item.id, type: 'class'}))
+            },
+            modules: {
+              data: jsonapidoc.data.filter(item => item.type === 'module').map(item => ({id: item.id, type: 'module'}))
+            },
+            project: {
+              data: {
+                id: 'ember',
+                type: 'project'
+              }
             }
-          }
+          },
         },
         included: [projectData]
       }
@@ -65,6 +70,10 @@ fetch()
       return RSVP.map(docs, function (doc) {
         console.log('updating ' + doc._id)
         return db.put(doc)
+      })
+    }).then(function () {
+      return RSVP.map(versions, (version) => {
+        return putClassesInCouch(tojsonapi(version.data), 'ember', version.version)
       })
     })
   }).catch(function (err) {
---
+++ test/update-ids-with-versions-and-project-test.js
@@ -0,0 +1,117 @@
+'use strict'
+
+let updateWithIDs = require('../lib/update-with-versions-and-project')
+let assert = require('chai').assert
+
+describe('update with versions and project', function () {
+
+  describe('`data` in jsonapi document is an object', function () {
+    beforeEach(function () {
+      this.document = {
+        data: {
+          id: 'Ember.CoreView',
+          type: 'class',
+          attributes: {
+            name: 'Ember.CoreView'
+          },
+          relationships: {
+            module: {
+              data: {
+                id: 'ember',
+                type: 'module'
+              }
+            }
+          }
+        },
+        included: [
+          {
+            data: {
+              id: 'ember',
+              type: 'module',
+              attributes: {
+                name: 'ember'
+              },
+              relationships: {
+                classes: {
+                  data: [
+                    {
+                      id: 'Ember.CoreView',
+                      type: 'class'
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        ]
+      }
+
+      this.converted = updateWithIDs(this.document, 'ember', '1.1.0')
+    })
+
+    it('adds the project name and version to the IDs', function () {
+      assert.equal(this.converted.data.id, 'ember-1.1.0-Ember.CoreView')
+      assert.equal(this.converted.included[0].data.id, 'ember-1.1.0-ember')
+      assert.equal(this.converted.included[0].data.relationships.classes.data[0].id, 'ember-1.1.0-Ember.CoreView')
+    })
+
+    it('adds the id to relationships', function () {
+      assert.equal(this.converted.data.relationships.module.data.id, 'ember-1.1.0-ember')
+    })
+  })
+
+  describe('when `data` is an array of objects in the jsonapi doc', function () {
+    beforeEach(function () {
+      this.document = {
+        data: [{
+          id: 'Ember.CoreView',
+          type: 'class',
+          attributes: {
+            name: 'Ember.CoreView'
+          },
+          relationships: {
+            module: {
+              data: {
+                id: 'ember',
+                type: 'module'
+              }
+            }
+          }
+        }],
+        included: [
+          {
+            data: {
+              id: 'ember',
+              type: 'module',
+              attributes: {
+                name: 'ember'
+              },
+              relationships: {
+                classes: {
+                  data: [
+                    {
+                      id: 'Ember.CoreView',
+                      type: 'class'
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        ]
+      }
+
+      this.converted = updateWithIDs(this.document, 'ember', '1.1.0')
+    })
+
+    it('adds the project name and version to the IDs', function () {
+      assert.equal(this.converted.data[0].id, 'ember-1.1.0-Ember.CoreView')
+      assert.equal(this.converted.included[0].data.id, 'ember-1.1.0-ember')
+      assert.equal(this.converted.included[0].data.relationships.classes.data[0].id, 'ember-1.1.0-Ember.CoreView')
+    })
+
+    it('adds the id to relationships', function () {
+      assert.equal(this.converted.data[0].relationships.module.data.id, 'ember-1.1.0-ember')
+    })
+  })
+})
