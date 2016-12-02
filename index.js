69ada20ccfdd804b9d991efff6c3027a003f3347
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2016-12-03 02:23:51 +0530

    Incremental docs publish, travis builds n more enhancements
    - Fetch needs to be run only once. Removing the redundant call
    - Added the ability to gen docs for a specific version
    - Travis build support
    - Generate log msgs so that travis doesn't timeout
    - Publish incremental docs ignoring existing versions of ember & ember-data in remote couch
    - Increase timeout between requests so that requests don't fail
    - Upgrade outdated npm deps, remove unused ones
    - Add an editorConfig
    - autoformat npm script to use standard's autofix feature

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 
 let RSVP = require('rsvp')
 let _ = require('lodash')
-var fetch = require('./lib/fetch')
-var readDocs = require('./lib/read-docs')
-var addSinceTags = require('./lib/add-since-tags')
-var addInheritedItems = require('./lib/add-inherited-items')
-var putClassesInCouch = require('./lib/classes-in-couch')
-var createVersionIndex = require('./lib/create-version-index')
-let normalizeEmberDependencies = require('./lib/normalize-ember-dependencies')
-let normalizeIDs = require('./lib/normalize-ids')
 let rm = require('rimraf')
 let PouchDB = require('pouchdb')
+let fs = require('fs')
+let glob = require('glob')
+
+let fetch = require('./lib/fetch')
+let readDocs = require('./lib/read-docs')
+let addSinceTags = require('./lib/add-since-tags')
+let addInheritedItems = require('./lib/add-inherited-items')
+let putClassesInCouch = require('./lib/classes-in-couch')
+let createVersionIndex = require('./lib/create-version-index')
+let normalizeEmberDependencies = require('./lib/normalize-ember-dependencies')
+let normalizeIDs = require('./lib/normalize-ids')
+let markup = require('./lib/markup')
+let batchUpdate = require('./lib/batch-update')
+
 require('marked')
 
 let db = new PouchDB(process.env.COUCH_URL, {
@@ -20,19 +26,14 @@ let db = new PouchDB(process.env.COUCH_URL, {
     password: process.env.COUCH_PASSWORD
   }
 })
-let fs = require('fs')
 
 if (fs.existsSync('tmp/docs')) {
   rm.sync('tmp/docs')
 }
 
-function fetchProject (projectName) {
-  console.log('downloading docs for ' + projectName)
-  let promise = fetch()
-    .then((stuff) => {
-      console.log('reading docs for ' + projectName)
-      return readDocs(projectName)
-    })
+function transformProjectFiles (projectName) {
+  console.log('reading docs for ' + projectName)
+  let promise = RSVP.resolve(readDocs(projectName))
     .then((stuff) => {
       console.log('adding since tags for ' + projectName)
       return addSinceTags(stuff)
@@ -47,33 +48,33 @@ function fetchProject (projectName) {
       return createVersionIndex(db, projectName, doc).then(() => doc)
     }).then(doc => {
       console.log('converting markdown to html for ' + projectName)
-
-      return require('./lib/markup')(doc)
+      return markup(doc)
     })
 
   return promise
 }
 
 let projects = ['ember', 'ember-data']
+let releaseToGenDocFor = process.argv[2] ? process.argv[2] : ''
 
-RSVP.map(projects, fetchProject).then(docs => {
-  let giantDocument = {
-    data: _.flatten(docs.map(doc => doc.data))
-  }
-  console.log('normalizing dependencies')
-  normalizeEmberDependencies(giantDocument)
+console.log('downloading docs for ' + projects.join(' & '))
 
-  return putClassesInCouch(giantDocument, db)
-}).then(function () {
-  let glob = require('glob')
+fetch(db, releaseToGenDocFor).then(downloadedFiles => {
+  RSVP.map(projects, transformProjectFiles).then(docs => {
+    let giantDocument = {
+      data: _.flatten(docs.map(doc => doc.data))
+    }
+    console.log('normalizing dependencies')
+    normalizeEmberDependencies(giantDocument)
 
-  let docs = glob.sync('tmp/docs/**/*.json')
+    return putClassesInCouch(giantDocument, db)
+  }).then(function () {
+    let docs = glob.sync('tmp/docs/**/*.json')
 
-  let batchUpdate = require('./lib/batch-update')
-
-  console.log('putting document in CouchDB')
-  return batchUpdate(db, docs)
-}).catch(function (err) {
-  console.warn('err!', err, err.stack)
-  process.exit(1)
+    console.log('putting document in CouchDB')
+    return batchUpdate(db, docs)
+  }).catch(function (err) {
+    console.warn('err!', err, err.stack)
+    process.exit(1)
+  })
 })
