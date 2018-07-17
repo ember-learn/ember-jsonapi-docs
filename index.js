5fbe174ddd828c9e726e9eb131d852f33ce9b50f
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-07-17 08:04:04 +0530

    Format existing files

--- index.js
+++ index.js
@@ -14,81 +14,81 @@ const revProjVersionFiles = require('./lib/rev-docs')
 const { downloadExistingDocsToLocal, uploadToS3 } = require('./lib/s3-sync')
 
 RSVP.on('error', function(reason) {
-  console.log(reason)
-  process.exit(1)
+	console.log(reason)
+	process.exit(1)
 })
 
 let possibleProjects = ['ember', 'ember-data']
 let projects =
-  argv.project && possibleProjects.includes(argv.project) ? [argv.project] : possibleProjects
+	argv.project && possibleProjects.includes(argv.project) ? [argv.project] : possibleProjects
 let specificDocsVersion = argv.version ? argv.version : ''
 
 let docsVersionMsg = specificDocsVersion !== '' ? '. For version ' + specificDocsVersion : ''
 console.log(`Downloading docs for ${projects.join(' & ')}${docsVersionMsg}`)
 
 downloadExistingDocsToLocal()
-  .then(() => fetchYuiDocs(projects, specificDocsVersion))
-  .then(() => readDocs(projects, specificDocsVersion))
-  .then(docs => {
-    return RSVP.map(projects, projectName => {
-      return RSVP.map(docs[projectName], doc => {
-        let docVersion = doc.version
-        console.log(`Starting to process ${projectName}-${docVersion}`)
-        return transformYuiObject([doc], projectName)
-          .then(markup)
-          .then(doc => {
-            let giantDocument = {
-              data: doc.data
-            }
-            console.log('normalizing dependencies')
-            return normalizeEmberDependencies(giantDocument)
-          })
-          .then(doc => {
-            return createClassesOnDisk(doc, projectName, docVersion)
-          })
-          .then(doc => {
-            console.log(`Finished processing ${projectName}-${docVersion}`)
-            return getVersionIndex(doc, projectName)
-          })
-          .then(doc => {
-            revProjVersionFiles(projectName, docVersion)
-            return doc
-          })
-      }).then(docs => {
-        let [docToSave, ...remainingDocs] = docs.filter(doc => doc.data.id === projectName)
+	.then(() => fetchYuiDocs(projects, specificDocsVersion))
+	.then(() => readDocs(projects, specificDocsVersion))
+	.then(docs => {
+		return RSVP.map(projects, projectName => {
+			return RSVP.map(docs[projectName], doc => {
+				let docVersion = doc.version
+				console.log(`Starting to process ${projectName}-${docVersion}`)
+				return transformYuiObject([doc], projectName)
+					.then(markup)
+					.then(doc => {
+						let giantDocument = {
+							data: doc.data
+						}
+						console.log('normalizing dependencies')
+						return normalizeEmberDependencies(giantDocument)
+					})
+					.then(doc => {
+						return createClassesOnDisk(doc, projectName, docVersion)
+					})
+					.then(doc => {
+						console.log(`Finished processing ${projectName}-${docVersion}`)
+						return getVersionIndex(doc, projectName)
+					})
+					.then(doc => {
+						revProjVersionFiles(projectName, docVersion)
+						return doc
+					})
+			}).then(docs => {
+				let [docToSave, ...remainingDocs] = docs.filter(doc => doc.data.id === projectName)
 
-        if (!docToSave) {
-          return Promise.resolve()
-        }
+				if (!docToSave) {
+					return Promise.resolve()
+				}
 
-        let existingDoc = `tmp/json-docs/${projectName}/projects/${projectName}.json`
-        if (fs.existsSync(existingDoc)) {
-          existingDoc = fs.readJsonSync(existingDoc)
-          docToSave.data.relationships['project-versions'].data = docToSave.data.relationships[
-            'project-versions'
-          ].data.concat(existingDoc.data.relationships['project-versions'].data)
-        }
+				let existingDoc = `tmp/json-docs/${projectName}/projects/${projectName}.json`
+				if (fs.existsSync(existingDoc)) {
+					existingDoc = fs.readJsonSync(existingDoc)
+					docToSave.data.relationships['project-versions'].data = docToSave.data.relationships[
+						'project-versions'
+					].data.concat(existingDoc.data.relationships['project-versions'].data)
+				}
 
-        remainingDocs.forEach(d => {
-          docToSave.data.relationships['project-versions'].data = docToSave.data.relationships[
-            'project-versions'
-          ].data.concat(d.data.relationships['project-versions'].data)
-        })
-        return saveDoc(docToSave, projectName).then(() => projectName)
-      })
-    })
-  })
-  .then(() => {
-    ;['ember', 'ember-data'].map(project => {
-      const projRevFile = `tmp/rev-index/${project}.json`
-      let projRevFileContent = fs.readJsonSync(`tmp/json-docs/${project}/projects/${project}.json`)
-      projRevFileContent.meta = {
-        availableVersions: []
-      }
-      projRevFileContent.data.relationships['project-versions'].data.forEach(pV =>
-        projRevFileContent.meta.availableVersions.push(pV.id.replace(`${project}-`, ''))
-      )
-      fs.writeJsonSync(projRevFile, projRevFileContent)
-    })
-  })
-  .then(uploadToS3)
+				remainingDocs.forEach(d => {
+					docToSave.data.relationships['project-versions'].data = docToSave.data.relationships[
+						'project-versions'
+					].data.concat(d.data.relationships['project-versions'].data)
+				})
+				return saveDoc(docToSave, projectName).then(() => projectName)
+			})
+		})
+	})
+	.then(() => {
+		;['ember', 'ember-data'].map(project => {
+			const projRevFile = `tmp/rev-index/${project}.json`
+			let projRevFileContent = fs.readJsonSync(`tmp/json-docs/${project}/projects/${project}.json`)
+			projRevFileContent.meta = {
+				availableVersions: []
+			}
+			projRevFileContent.data.relationships['project-versions'].data.forEach(pV =>
+				projRevFileContent.meta.availableVersions.push(pV.id.replace(`${project}-`, ''))
+			)
+			fs.writeJsonSync(projRevFile, projRevFileContent)
+		})
+	})
+	.then(uploadToS3)
--- test/filter-jsonapi-doc-test.js
+++ test/filter-jsonapi-doc-test.js
@@ -4,71 +4,67 @@ let assert = require('chai').assert
 let _ = require('lodash')
 let classWithIncluded = require('../lib/filter-jsonapi-doc').classWithIncluded
 
-describe('filter json api docs', function () {
-  describe('#classWithIncluded', function () {
-    beforeEach(function () {
-      this.document = {
-        data: [
-          {
-            id: 'Ember.Butt',
-            type: 'class',
-            relationships: {
-              methods: [
-                {id: 'Ember.Butt#Foo', type: 'method'}
-              ]
-            }
-          },
-          {
-            id: 'NotAClass',
-            type: 'yoloswag'
-          }
-        ],
-        included: [
-          {
-            id: 'Ember.Butt#foo',
-            type: 'method',
-            relationships: {
-              class: {
-                data: { id: 'Ember.Butt', type: 'class' }
-              }
-            }
-          }
-        ]
-      }
+describe('filter json api docs', function() {
+	describe('#classWithIncluded', function() {
+		beforeEach(function() {
+			this.document = {
+				data: [
+					{
+						id: 'Ember.Butt',
+						type: 'class',
+						relationships: {
+							methods: [{ id: 'Ember.Butt#Foo', type: 'method' }]
+						}
+					},
+					{
+						id: 'NotAClass',
+						type: 'yoloswag'
+					}
+				],
+				included: [
+					{
+						id: 'Ember.Butt#foo',
+						type: 'method',
+						relationships: {
+							class: {
+								data: { id: 'Ember.Butt', type: 'class' }
+							}
+						}
+					}
+				]
+			}
 
-      this.response = classWithIncluded(this.document, 'Ember.Butt')
-    })
+			this.response = classWithIncluded(this.document, 'Ember.Butt')
+		})
 
-    it('returns the class in the result', function () {
-      assert.typeOf(this.response.data, 'object')
-      assert.deepEqual(this.response.data, {
-        id: 'Ember.Butt',
-        type: 'class',
-        relationships: {
-          methods: [
-            {id: 'Ember.Butt#Foo', type: 'method'}
-          ]
-        }
-      })
-    })
+		it('returns the class in the result', function() {
+			assert.typeOf(this.response.data, 'object')
+			assert.deepEqual(this.response.data, {
+				id: 'Ember.Butt',
+				type: 'class',
+				relationships: {
+					methods: [{ id: 'Ember.Butt#Foo', type: 'method' }]
+				}
+			})
+		})
 
-    it('returns associated methods', function () {
-      assert.deepEqual(this.response.included, [
-        {
-          id: 'Ember.Butt#foo',
-          type: 'method',
-          relationships: {
-            class: {
-              data: { id: 'Ember.Butt', type: 'class' }
-            }
-          }
-        }
-      ])
-    })
+		it('returns associated methods', function() {
+			assert.deepEqual(this.response.included, [
+				{
+					id: 'Ember.Butt#foo',
+					type: 'method',
+					relationships: {
+						class: {
+							data: { id: 'Ember.Butt', type: 'class' }
+						}
+					}
+				}
+			])
+		})
 
-    it('does not return unassociated models', function () {
-      let unassociated = _.filter(this.response.included, ['type', 'yoloswag'])
-      assert.equal(unassociated.length, 0)
-    })
-  })
+		it('does not return unassociated models', function() {
+			let unassociated = _.filter(this.response.included, ['type', 'yoloswag'])
+			assert.equal(unassociated.length, 0)
+		})
+	})
 })
