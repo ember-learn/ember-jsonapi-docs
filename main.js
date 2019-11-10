56ad8fdd0890521ea69a26f5e2bc14089c00323a
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-11-10 21:13:45 +0530

    Refactor to simplify files

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 import RSVP from 'rsvp'
-import fs from 'fs-extra'
-import rimraf from 'rimraf'
-
-import markup from './lib/markup'
+import { filler1 } from './lib/filler1'
+import { filler2 } from './lib/filler2'
 import readDocs from './lib/read-docs'
-
-import createClassesOnDisk from './lib/create-classes'
-import transformYuiObject from './lib/transform-yui-object'
-import normalizeEmberDependencies from './lib/normalize-ember-dependencies'
-import getVersionIndex from './lib/get-version-index'
-import saveDoc from './lib/save-document'
-import revProjVersionFiles from './lib/rev-docs'
+import { revProjectDocs } from './lib/rev-project-docs'
 import { uploadDocsToS3 } from './lib/s3-sync'
+
+const supportedProjects = ['ember', 'ember-data']
 
 export async function apiDocsProcessor(projects, specificDocsVersion) {
 	let docsVersionMsg = specificDocsVersion !== '' ? `. For version ${specificDocsVersion}` : ''
@@ -19,76 +13,13 @@ export async function apiDocsProcessor(projects, specificDocsVersion) {
 
 	let docs = await readDocs(projects, specificDocsVersion)
 
-	return RSVP.map(projects, projectName => {
-		return RSVP.map(docs[projectName], doc => {
-			let docVersion = doc.version
-			console.log(`Starting to process ${projectName}-${docVersion}`)
-
-			const existingFolder = `tmp/json-docs/${projectName}/${docVersion}`
-			if (fs.existsSync(existingFolder)) {
-				rimraf.sync(existingFolder)
-			}
-
-			return transformYuiObject([doc], projectName)
-				.then(markup)
-				.then(({ data }) => {
-					let giantDocument = { data }
-					console.log('normalizing dependencies')
-					return normalizeEmberDependencies(giantDocument)
-				})
-				.then(doc => {
-					return createClassesOnDisk(doc, projectName, docVersion)
-				})
-				.then(doc => {
-					console.log(`Finished processing ${projectName}-${docVersion}`)
-					return getVersionIndex(doc, projectName)
-				})
-				.then(doc => {
-					revProjVersionFiles(projectName, docVersion)
-					return doc
-				})
-		}).then(docs => {
-			let [docToSave, ...remainingDocs] = docs.filter(({ data }) => data.id === projectName)
-
-			if (!docToSave) {
-				return Promise.resolve()
-			}
-
-			let existingDoc = `tmp/json-docs/${projectName}/projects/${projectName}.json`
-			if (fs.existsSync(existingDoc)) {
-				existingDoc = fs.readJsonSync(existingDoc)
-				docToSave.data.relationships['project-versions'].data = docToSave.data.relationships[
-					'project-versions'
-				].data.concat(existingDoc.data.relationships['project-versions'].data)
-			}
-
-			remainingDocs.forEach(({ data }) => {
-				docToSave.data.relationships['project-versions'].data = docToSave.data.relationships[
-					'project-versions'
-				].data.concat(data.relationships['project-versions'].data)
-			})
-			return saveDoc(docToSave, projectName).then(() => projectName)
-		})
-	})
-
-		.then(() =>
-			['ember', 'ember-data'].map(project => {
-				const projRevFile = `tmp/rev-index/${project}.json`
-				let projRevFileContent = fs.readJsonSync(
-					`tmp/json-docs/${project}/projects/${project}.json`
-				)
-				projRevFileContent.meta = {
-					availableVersions: [],
-				}
-				projRevFileContent.data.relationships['project-versions'].data.forEach(({ id }) =>
-					projRevFileContent.meta.availableVersions.push(id.replace(`${project}-`, ''))
-				)
-				fs.writeJsonSync(projRevFile, projRevFileContent)
-			})
+	await RSVP.map(projects, projectName =>
+		RSVP.map(docs[projectName], doc => filler2(projectName, doc)).then(docs =>
+			filler1(projectName, docs)
 		)
-		.then(uploadDocsToS3)
-		.then(() => {
-			console.log('\n\n\n')
-			console.log('Done!')
-		})
+	)
+
+	await revProjectDocs(supportedProjects)
+
+	return await uploadDocsToS3()
 }
