2b3a6274b27930edec8c10dd4b23a9a3a58affb1
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-11-10 22:23:59 +0530

    remove yuidoc fixer docs

--- src/lib/yuidoc-fixer/emberKnownEntityNames.js
+++
@@ -1,5 +0,0 @@
-import moduleData from 'ember-rfc176-data'
-import keyfinder from 'keyfinder'
-import { uniq } from 'lodash'
-
-export const emberKnownEntityNames = uniq(keyfinder(moduleData, 'export'))
