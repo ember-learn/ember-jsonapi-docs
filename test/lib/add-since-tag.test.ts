9ff82c756f5f12e68ddeb825d68e28d3ce5e6bee
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-11-10 22:41:09 +0530

    bring back old tests

---
+++ test/lib/add-since-tag.test.ts
@@ -0,0 +1,34 @@
+import addSinceTags from '../../src/lib/add-since-tags'
+import { assert } from 'chai'
+import * as _ from 'lodash'
+
+describe('addSinceTags', () => {
+	beforeEach(function() {
+		this.yuiDocSets = _.range(3).map(i => {
+			return {
+				version: `v1.0.${i}`,
+				data: {
+					classitems: [
+						{
+							itemtype: 'method',
+							name: 'foo',
+							class: 'Foo',
+						},
+						{
+							itemtype: 'method',
+							name: 'bar',
+							class: 'Foo',
+						},
+					],
+				},
+			}
+		})
+		addSinceTags(this.yuiDocSets)
+	})
+
+	it('adds a since tag to classitems', function() {
+		this.yuiDocSets.forEach(({ data }) => {
+			data.classitems.forEach(({ since }) => assert.equal(since, '1.0.0'))
+		})
+	})
+})
---
+++ test/lib/modules-transform.test.ts
@@ -0,0 +1,107 @@
+import transformModules from '../../src/lib/modules-transform'
+import { assert } from 'chai'
+import * as _ from 'lodash'
+
+describe('transformModules', () => {
+	beforeEach(function() {
+		this.yuiDocSets = _.range(3).map(i => {
+			return {
+				version: `v1.0.${i}`,
+				data: {
+					modules: [
+						{
+							is_submodule: 1,
+							module: 'foo',
+							submodules: {},
+							classes: {
+								'Testing.class.private-1': 1,
+								'Testing.class.public-2': 1,
+								'Testing.class.private-3': 1,
+								'Testing.class.public-4': 1,
+								'Testing.class.deprecated-3': 1,
+							},
+						},
+						{
+							is_submodule: 1,
+							module: 'foo',
+							submodules: {},
+							classes: {
+								'Testing.class.private-1': 1,
+							},
+						},
+						{
+							is_submodule: 0,
+							module: 'bar',
+							submodules: {},
+							classes: {
+								'Testing.class.private-1': 1,
+								'Testing.class.public-2': 1,
+							},
+						},
+					],
+					classes: {
+						'Testing.class.private-1': {
+							file: 'testing/class/private/1.js',
+							access: 'private',
+							deprecated: false,
+						},
+						'Testing.class.public-2': {
+							file: 'testing/class/public/2.js',
+							access: 'public',
+							deprecated: false,
+						},
+						'Testing.class.private-3': {
+							file: 'testing/class/private/3.js',
+							access: 'private',
+							deprecated: false,
+						},
+						'Testing.class.public-4': {
+							file: 'testing/class/public/4.js',
+							access: 'public',
+							deprecated: false,
+						},
+						'Testing.class.deprecated-3': {
+							file: 'testing/class/deprecated/3.js',
+							access: 'public',
+							deprecated: true,
+						},
+					},
+				},
+			}
+		})
+		transformModules(this.yuiDocSets)
+	})
+
+	it('adds a parent attribute to sub modules', function() {
+		this.yuiDocSets.forEach(({ data }) => {
+			let subModules = _.filter(data.modules, ({ is_submodule }) => is_submodule)
+			subModules.forEach(({ parent }) => {
+				assert.equal(parent, 'foo')
+			})
+		})
+	})
+
+	it('publicclasses/privateclasses attributes are set correctly', function() {
+		this.yuiDocSets.forEach(({ data }) => {
+			let modules = data.modules
+			assert.deepEqual(modules[0].publicclasses, [
+				'Testing.class.public-2',
+				'Testing.class.public-4',
+			])
+			assert.deepEqual(modules[0].privateclasses, [
+				'Testing.class.private-1',
+				'Testing.class.private-3',
+				'Testing.class.deprecated-3',
+			])
+			assert.isUndefined(modules[0].classes)
+
+			assert.deepEqual(modules[1].publicclasses, [])
+			assert.deepEqual(modules[1].privateclasses, ['Testing.class.private-1'])
+			assert.isUndefined(modules[1].classes)
+
+			assert.deepEqual(modules[2].publicclasses, ['Testing.class.public-2'])
+			assert.deepEqual(modules[2].privateclasses, ['Testing.class.private-1'])
+			assert.isUndefined(modules[2].classes)
+		})
+	})
+})
