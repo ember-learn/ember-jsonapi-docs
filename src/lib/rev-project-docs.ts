d069dd3d8ac28ce87816198d477f73a8cd652ef9
Sivakumar Kailasam <siva@rubberduck.engineering>
2019-11-11 16:40:59 +0530

    sort available versions in meta section

--- src/lib/rev-project-docs.ts
+++ src/lib/rev-project-docs.ts
@@ -1,4 +1,5 @@
 import * as SafePromise from 'bluebird'
+import * as compareVersions from 'compare-versions'
 import * as fs from 'fs-extra'
 
 export async function revProjectDocs(projects) {
@@ -13,6 +14,10 @@ export async function revProjectDocs(projects) {
 
 		projRevFileContent.data.relationships['project-versions'].data.forEach(({ id }) =>
 			projRevFileContent.meta.availableVersions.push(id.replace(`${project}-`, ''))
+		)
+
+		projRevFileContent.meta.availableVersions = projRevFileContent.meta.availableVersions.sort(
+			compareVersions
 		)
 
 		return fs.writeJson(projRevFile, projRevFileContent)
