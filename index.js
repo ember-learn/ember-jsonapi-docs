2005ba528eeb661026fabb9e00f12b35099cfb38
Chris Manson <chris@manson.ie>
2023-11-10 20:47:41 +0000

    move from minimist to commander

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-import minimist from 'minimist';
+#!/usr/bin/env node
+
 import { apiDocsProcessor } from './main.js';
+import { program, Option, InvalidArgumentError } from 'commander';
+import chalk from 'chalk';
 
-const argv = minimist(process.argv.slice(2));
+function semverVersion(value) {
+  if (!/^\d+\.\d+\.\d+$/.test(value)) {
+    throw new InvalidArgumentError('Not a correctly defined semver version i.e. major.minor.patch');
+  }
+  return value;
+}
 
-let possibleProjects = ['ember', 'ember-data'];
-let projects =
-  argv.project && possibleProjects.includes(argv.project) ? [argv.project] : possibleProjects;
-let specificDocsVersion = argv.version ? argv.version : '';
+program
+  .addOption(
+    new Option('-p, --projects <project...>', 'the projects that you want to run this for')
+      .choices(['ember', 'ember-data'])
+      .makeOptionMandatory(),
+  )
+  .addOption(
+    new Option('-v, --version <version>', 'project version', semverVersion).conflicts('all'),
+  )
+  .addOption(
+    new Option('-a, --all', 'process all versions (this will take a long time)').conflicts(
+      'version',
+    ),
+  )
+  .option('-c, --clean', 'clean (not sure what this does)');
 
-let runClean = !!argv.clean;
-let noSync = !argv.sync;
+program.parse();
 
-apiDocsProcessor(projects, specificDocsVersion, runClean, noSync);
+const options = program.opts();
+
+const { projects, version, clean, all } = options;
+
+if (!version && !all) {
+  console.log(chalk.red('You need to specify a --version or pass --all to this program'));
+  process.exit(1);
+}
+
+apiDocsProcessor(projects, version, clean);
