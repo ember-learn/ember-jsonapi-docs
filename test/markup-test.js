fce166c7fe41768ad15d7920db2e477a3e949c42
Chris Manson <chris@manson.ie>
2024-07-05 14:23:09 +0100

    simplifying the markup test

--- test/markup-test.js
+++ test/markup-test.js
@@ -1,58 +1,19 @@
-import markup from '../lib/markup.js';
+import { fixFilename } from '../lib/markup.js';
 import { assert } from 'chai';
-import fs from 'fs';
+import fsExtra from 'fs-extra';
+import { join } from 'path';
 
-function desc(path) {
-  return {
-    id: Date.now(),
-    attributes: {
-      description: fs.readFileSync(`./test/mocks/description/${path}.md`, 'utf-8'),
-      methods: [],
-      properties: [],
-      events: [],
-    },
-  };
-}
+const { readFileSync, readdirSync } = fsExtra;
 
-function mark(path) {
-  let data = [desc(path)];
-  let result = markup({ data });
-  let content = result.data[0].attributes.description;
-  maybeWrite(content, path);
-  return content;
-}
-
-function maybeWrite(content, path) {
-  const fsPath = `./test/mocks/description/${path}.html`;
-  if (fs.existsSync(fsPath)) {
-    return;
-  }
-  fs.writeFileSync(fsPath, content, 'utf-8');
-}
-
-function snapshot(path) {
-  const fsPath = `./test/mocks/description/${path}.html`;
-  return fs.readFileSync(fsPath, 'utf8');
-}
+const inputFiles = readdirSync('./test/mocks/input');
 
 describe('markup', () => {
-  it('render correct syntax for handlebars with title', function () {
-    const caseName = 'handlebars-title';
-    assert.equal(mark(caseName), snapshot(caseName));
-  });
+  for (let file of inputFiles) {
+    it(`render correct syntax for ${file}`, function () {
+      const inputFile = readFileSync(join('./test/mocks/input', file), 'utf-8');
+      const outputFile = readFileSync(join('./test/mocks/output', file), 'utf-8');
 
-  it('render correct syntax for handlebars without title', function () {
-    const caseName = 'handlebars';
-    assert.equal(mark(caseName), snapshot(caseName));
-  });
-
-  it('render correct syntax for javascript with title', function () {
-    const caseName = 'javascript-title';
-    assert.equal(mark(caseName), snapshot(caseName));
-  });
-
-  it('render correct syntax for javascript without title', function () {
-    const caseName = 'javascript';
-    assert.equal(mark(caseName), snapshot(caseName));
-  });
+      assert.equal(fixFilename(inputFile), outputFile);
+    });
+  }
 });
